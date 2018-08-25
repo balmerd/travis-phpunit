f7bac42812ac697ff739dc3421a174930b0c89dc
David Balmer <david.balmer@transsight.com>
2018-08-25 10:08:07 -0700

    first commit

---
+++ tests/phpunit/example.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class DavePHPUnitTest extends TestCase
+{
+    public function testEquals(): void
+    {
+        $this->assertEquals('foo', 'foo');
+        // $this->assertEquals('foo', 'bar');
+    }
+}
